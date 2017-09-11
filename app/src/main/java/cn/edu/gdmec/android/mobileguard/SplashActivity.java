2219afa5335173f6bffd8f356e7d781723c6b4f9
MelaJia <838672863@qq.com>
2017-09-11 16:40:04 +0800

    陈佳虹修改一次

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
@@ -11,6 +11,6 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         System.out.print("你好啊！");
-       
+
     }
 }
